580660c4bc7b89e6f757f040b8aa0f08972e292f
ievgenii Bloshchanevych <mkgeka@gmail.com>
2023-02-04 15:20:45 -0500

    add python

--- resources/my_file.py
+++ resources/my_file.py
@@ -3,7 +3,6 @@ def all(iterable):
     melement = 0
     for element in range(1,iterable):
         melement = melement + element
-        print(melement)
     return melement
 output = all(50)
 print(output)
