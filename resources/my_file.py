1181a0c1a9f144adf13a8b0937fa98886a357beb
ievgenii Bloshchanevych <mkgeka@gmail.com>
2023-02-04 14:46:00 -0500

    add python

--- resources/my_file.py
+++ resources/my_file.py
@@ -1,2 +1,13 @@
 #!/usr/bin/env python
-print("Hello")
+from typing import List
+
+def check(row_start:int, row_end:int, column_start:int, column_end:int) -> List[List[int]]:
+    M = [[*range(row_start, row_end+1, 1)], [*range(column_start, column_end+1, 1)]]
+    list = []
+    for i in M[0]:
+        Result = [item * i for item in M[1]]
+        list.append(Result)
+    pass
+    return list
+output = check(2,4,3,7)
+print(output)
