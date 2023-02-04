31d53fb174f02f1fa9dbed3e601c298f15826557
ievgenii Bloshchanevych <mkgeka@gmail.com>
2023-02-04 14:53:53 -0500

    add python

--- resources/my_file.py
+++ resources/my_file.py
@@ -1,15 +1,5 @@
 #!/usr/bin/env python
 def replacer(s: str) -> str:
-    message = ""
-    for symbol in s:
-        if symbol.find("\"") != -1:
-            character = symbol.replace("\"", "\'")
-            message = message + character
-        elif symbol.find("\'") != -1:
-            character = symbol.replace("\'", "\"")
-            message = message + character
-        else:
-            message = message + symbol
-    return message
-output = replacer('\'"\'\'"\'\'"\'\'"\'\'"\'\'"\'\'"\'')
+    return s
+output = replacer('Hello World!!!')
 print(output)
