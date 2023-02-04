6393abbd38349b81bc20f0e445d427cb96e7587e
ievgenii Bloshchanevych <mkgeka@gmail.com>
2023-02-04 15:22:52 -0500

    add python

--- resources/my_file.py
+++ resources/my_file.py
@@ -3,7 +3,7 @@ def all(iterable):
     melement = 0
     for element in range(1,iterable):
         print(element)
-        melement = melement + element
+        melement = melement * element
     return melement
 output = all(10)
 print(output)
